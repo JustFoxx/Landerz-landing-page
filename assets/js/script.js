7e52c8ff8bdf16646b3affeb21a6fe6d289eeb8e
Dolesyak <90189726+Dolesyak@users.noreply.github.com>
2022-10-17 00:03:32 +0200

    Version: 0.6.1
    - Full Responsive
    - Dark mode switch works

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+    $('.hamburger').on('click', () => {
+        $('.hamburger').toggleClass('active')
+    })
+
+    $('.dmode-toggle').on('click', () => {
+        $('body').toggleClass('darkmode')
+    })
